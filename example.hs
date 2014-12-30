169d64b7833b6d438d3a599e5c59758c1ca1c9a2
notogawa <n.ohkawa@gmail.com>
2014-12-30 16:31:49 +0900

    example

---
+++ example.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
+module Main where
+
+import Text.Haiji
+import Text.Haiji.Types
+import Text.Haiji.TH
+import qualified Data.Text.Lazy as LT
+import qualified Data.Text.Lazy.IO as LT
+
+main :: IO ()
+main = LT.putStr $ render HTML dict $(haijiFile "example.tmpl") where
+    dict :: TLDict
+            '[ "a_variable" :-> LT.Text
+             , "navigation" :-> [ TLDict
+                                  '[ "href" :-> LT.Text
+                                   , "caption" :-> LT.Text
+                                   ]
+                                ]
+             , "foo" :-> Int
+             , "bar" :-> LT.Text
+             ]
+    dict = Ext (Value "Hello,World!") $
+           Ext (Value [ Ext (Value "content/a.html") $ Ext (Value "A") Empty
+                      , Ext (Value "content/b.html") $ Ext (Value "B") Empty
+                      ]
+               ) $
+           Ext (Value 1) $
+           Ext (Value "") Empty
