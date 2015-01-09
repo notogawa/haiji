e49d65a5fb8b372ce166e91775e6d7d1a2dec17d
notogawa <n.ohkawa@gmail.com>
2015-01-10 01:28:33 +0900

    define ToJSON TLDict instance

--- example.hs
+++ example.hs
@@ -2,14 +2,6 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeOperators #-}
-
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE PolyKinds #-}
-{-# LANGUAGE GADTs #-}
-{-# LANGUAGE OverlappingInstances #-}
-
 module Main where
 
 import Text.Haiji
@@ -17,33 +9,6 @@ import Text.Haiji.Types
 import qualified Data.Text as T
 import qualified Data.Text.Lazy as LT
 import qualified Data.Text.Lazy.IO as LT
-import GHC.TypeLits
-
-class ToPython a where
-  toPython  :: a -> LT.Text
-  toPython' :: a -> LT.Text
-  toPython'= toPython
-instance ToPython (TLDict '[]) where
-  toPython  Empty = "{}"
-  toPython' Empty = " }"
-instance (ToPython (TLDict s), ToPython kv) => ToPython (TLDict (kv ': s)) where
-  toPython  (Ext x xs) = LT.concat ["{ ", toPython x, toPython' xs]
-  toPython' (Ext x xs) = LT.concat [", ", toPython x, toPython' xs]
-instance (ToPython v, KnownSymbol k) => ToPython (k :-> v) where
-  toPython  x = LT.concat [LT.pack (show $ key x), ": ", toPython (value x)]
-instance ToPython v => ToPython [v] where
-  toPython  [] = "[]"
-  toPython  (x:xs) = LT.concat ["[ ", toPython x, toPython' xs]
-  toPython' [] = " ]"
-  toPython' (x:xs) = LT.concat [", ", toPython x, toPython' xs]
-instance ToPython String where
-  toPython  = LT.pack . show
-instance ToPython T.Text where
-  toPython = LT.pack . show
-instance ToPython LT.Text where
-  toPython = LT.pack . show
-instance ToPython Int where
-  toPython = LT.pack . show
 
 main :: IO ()
 main = LT.putStr $ render HTML (asTLDict dict) $(haijiFile "example.tmpl")
--- test/tests.hs
+++ test/tests.hs
@@ -7,11 +7,15 @@ module Main where
 import Test.Tasty.TH
 import Test.Tasty.HUnit
 
+import Data.Aeson
 import Text.Haiji
 import Text.Haiji.Types
 import qualified Data.Text as T
 import qualified Data.Text.Lazy as LT
+import qualified Data.Text.Lazy.Encoding as LT
 import System.Process.Text.Lazy
+
+import Test.Util ()
 
 main :: IO ()
 main = $(defaultMainGenerator)
@@ -22,27 +26,17 @@ case_example = do
                         $ LT.unlines [ "from jinja2 import Environment, PackageLoader"
                                      , "env = Environment(loader=PackageLoader('example', '.'))"
                                      , "template = env.get_template('example.tmpl')"
-                                     , "print template.render("
-                                     , "  {'a_variable' : \"Hello,World!\","
-                                     , "  'navigation' : [ { 'href': 'content/a.html', 'caption': 'A'},"
-                                     , "                 { 'href': 'content/b.html', 'caption': 'B'} ] }"
-                                     , ")"
+                                     , "print template.render(", LT.decodeUtf8 (encode dict), ")"
                                      ]
   out @=? render HTML (asTLDict dict) $(haijiFile "example.tmpl") where
-    dict :: TLDict
-            '[ "a_variable" :-> T.Text
-             , "navigation" :-> [ TLDict
-                                  '[ "caption" :-> LT.Text
-                                   , "href" :-> String
-                                   ]
-                                ]
-             , "foo" :-> Int
-             , "bar" :-> LT.Text
-             ]
-    dict = Ext (Value "Hello,World!") $
+    dict = Ext (Value "Hello,World!" :: "a_variable" :-> T.Text) $
            Ext (Value [ Ext (Value "A") $ Ext (Value "content/a.html") $ Empty
                       , Ext (Value "B") $ Ext (Value "content/b.html") $ Empty
-                      ]
+                      ] :: "navigation" :-> [ TLDict
+                                              '[ "caption" :-> LT.Text
+                                               , "href" :-> String
+                                               ]
+                                            ]
                ) $
-           Ext (Value 1) $
-           Ext (Value "") Empty
+           Ext (Value 1 :: "foo" :-> Int) $
+           Ext (Value "" :: "bar" :-> LT.Text) Empty
