1a6351322a83b2a2901ed81312bb8cc15d1bed84
notogawa <n.ohkawa@gmail.com>
2015-11-05 00:42:42 +0900

    export only main

--- test/tests.hs
+++ test/tests.hs
@@ -2,7 +2,7 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE DataKinds #-}
-module Main where
+module Main ( main ) where
 
 import Test.Tasty.TH
 import Test.Tasty.HUnit
